35a6be13885fa0aa7dd5a2a5023a14a6d1a26728
GITSangWoo <dogcenta@gmail.com>
2024-08-30 12:12:48 +0900

    simple code

--- src/fishmlserv/main.py
+++ src/fishmlserv/main.py
@@ -8,7 +8,30 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/fish")
+def fish(length:float, weight:float):
+    """
+    물고기의 종류 판별기
+    
+    Args:
+        legth (float) : 물고기 길이(cm)\
+        weight(float) : 물고기 무기(g)
+    
+    Returns:
+        dict: 물고기 종류를 담은 딕셔너리
+        
+    """
+    if length > 30.0:
+        prediction = "도미"
+    else :
+        prediction = "빙어"
+
+    return {
+                "prediction" : prediction,
+                "length" : length,
+                "weight": weight
+            }
