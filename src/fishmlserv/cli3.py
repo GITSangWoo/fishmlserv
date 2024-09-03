d65e24969173d459e5e5e9f84e8af5748423d104
GITSangWoo <dogcenta@gmail.com>
2024-09-03 12:35:05 +0900

    add dependencies

---
+++ src/fishmlserv/cli3.py
@@ -0,0 +1,30 @@
+import requests
+import json 
+import typer
+
+
+headers = {
+            'accept': 'application/json',
+        }
+
+def main(l:int=0, w:int=0):
+    if l :
+        le=l
+        if w:
+            we=w
+            params = {
+                'length': le,
+                'weight': we,
+                }
+            response = requests.get('http://127.0.0.1:8000/fish_ml_predictor', params=params, headers=headers)
+            data=json.loads(response.text) 
+            typer.echo(f"{data['prediction']}") 
+              
+        else:
+            typer.echo("다시 ㄱ")
+    else : 
+        typer.echo("다시 ㄱ")
+
+def run_main():
+    typer.run(main)
+
