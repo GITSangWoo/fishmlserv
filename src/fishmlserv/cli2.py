649c2ce90a1abbdbcf91c71ac59aaead8ab04a5c
GITSangWoo <dogcenta@gmail.com>
2024-09-03 14:01:00 +0900

    good

--- src/fishmlserv/cli2.py
+++ src/fishmlserv/cli2.py
@@ -1,5 +1,5 @@
 import requests
-import json 
+import json
 import typer
 
 
@@ -9,20 +9,21 @@ headers = {
 
 def main(l:float=typer.Option(...,"--length","-l"), w:float=typer.Option(...,"--weight","-w")):
     if l :
+        le=l
         if w:
+            we=w
             params = {
-                'length': l,
-                'weight': w,
+                'length': le,
+                'weight': we,
                 }
             response = requests.get('http://127.0.0.1:8000/fish_ml_predictor', params=params, headers=headers)
-            data=json.loads(response.text) 
-            typer.echo(f"{data['prediction']}") 
-              
+            data=json.loads(response.text)
+            typer.echo(f"{data['prediction']}")
+
         else:
             typer.echo("다시 ㄱ")
-    else : 
+    else :
         typer.echo("다시 ㄱ")
-
 
 def run_main():
     typer.run(main)
