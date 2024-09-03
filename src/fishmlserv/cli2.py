f26ef67a27c5f31f79db12e918116e60c9483922
GITSangWoo <dogcenta@gmail.com>
2024-09-03 14:08:48 +0900

    server fix

--- src/fishmlserv/cli2.py
+++ src/fishmlserv/cli2.py
@@ -16,7 +16,7 @@ def main(l:float=typer.Option(...,"--length","-l"), w:float=typer.Option(...,"--
                 'length': le,
                 'weight': we,
                 }
-            response = requests.get('http://127.0.0.1:8000/fish_ml_predictor', params=params, headers=headers)
+            response = requests.get('http://127.0.0.1:8080/fish_ml_predictor', params=params, headers=headers)
             data=json.loads(response.text)
             typer.echo(f"{data['prediction']}")
 
